42fb9944d242fb06ff8116ba18f970f04daf41b2
kornel (Kornelius Rohmeyer) <rohmeyer@small-projects.de>
2009-07-27 15:30:39 +0000

    JUnitTest for RList
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/jhlir@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/junit/RList_Test.java
+++ src/junit/RList_Test.java
@@ -8,38 +8,66 @@ import org.junit.Test;
 
 public class RList_Test extends MyTestCase {
     protected RList rList1;
-
+    protected RList rList2;
+    protected RList rList3;
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
         getRServices().assign("rlist1", "list(xx=1:3, yy=list(), matrix(c(1,2,3,4),4,1), zz=\"foo\")");
         rList1 = getRServices().eval("rlist1").asRList();
+        getRServices().assign("rlist2", "list(1:3, list(1,2,3), matrix(c(1,2,3,4),4,1), \"foo\")");
+        rList2 = getRServices().eval("rlist2").asRList();
+        getRServices().assign("rlist3", "list()");
+        rList3 = getRServices().eval("rlist3").asRList();
+        
     }
 
     @Test
     public void testAt()  {
         assertEquals(rList1.getLength(), 4);
+        assertEquals(rList2.getLength(), 4);
+        assertEquals(rList3.getLength(), 0);
 
         assertEquals(rList1.getName(0), "xx");
         assertEquals(rList1.getName(1), "yy");
+        assertEquals(rList1.getName(2), null);
         assertEquals(rList1.getName(3), "zz");
 
+        assertEquals(rList1.getName(0), null);
+        assertEquals(rList1.getName(1), null);
+        assertEquals(rList1.getName(2), null);
+        assertEquals(rList1.getName(3), null);        
+        
         assertEquals(rList1.getNames()[0], "xx");
         assertEquals(rList1.getNames()[1], "yy");
         assertEquals(rList1.getNames()[3], "zz");
-
-        assertEquals(rList1.getNames()[0], "xx");
-        assertEquals(rList1.getNames()[1], "yy");
-        assertEquals(rList1.getNames()[3], "zz");
-
+        
+        assertEquals(rList2.getNames(), null);
+        assertEquals(rList3.getNames(), null);
+        
+        assertTrue(rList1.get("xx") instanceof RInteger);
+        assertTrue(rList1.get("yy") instanceof RList);        
+        assertTrue(rList1.get("zz") instanceof RChar);
+        
         assertTrue(rList1.get(0) instanceof RInteger);
         assertTrue(rList1.get(1) instanceof RList);
         assertTrue(rList1.get(2) instanceof RMatrix);
         assertTrue(rList1.get(3) instanceof RChar);
+        
+        assertEquals(rList1.getNamesAsList().size(), 4);
+        assertEquals(rList1.getNamesAsList().get(0), "xx");
+    }    
 
-
-
+    @Test(expected=IndexOutOfBoundsException.class)
+    public void testIndexOutOfBoundsException1() {
+    	rList3.get(1);
     }
+
+    @Test(expected=IndexOutOfBoundsException.class)
+    public void testIndexOutOfBoundsException2() {
+    	rList1.get("blah");
+    }
+
 
 }
