b808c4ec84d8c5bbd60c1344e5bb29fc56c3327d
kornel (Kornelius Rohmeyer) <rohmeyer@small-projects.de>
2009-07-29 11:57:57 +0000

    RFactor is no RVector.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/jhlir@32 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/jhlir/RFactor.java
+++ src/jhlir/RFactor.java
@@ -2,7 +2,7 @@ package jhlir;
 
 import java.util.List;
 
-public interface RFactor<WRAPPED_TYPE> extends RVector<WRAPPED_TYPE, String[], String>{
+public interface RFactor<WRAPPED_TYPE> extends RVectorFactor<WRAPPED_TYPE, String[], String>{
     String[] getData();
     String[] getDataAsObjArr();
     List<String> getDataAsList();
