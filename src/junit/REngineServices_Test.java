414b2494d848d3bc19abd3491a3b3b09e1c78095
kornel (Kornelius Rohmeyer) <rohmeyer@small-projects.de>
2009-07-30 10:09:21 +0000

    REngineServices_Test for errors and warnings.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/jhlir@36 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/junit/REngineServices_Test.java
+++ src/junit/REngineServices_Test.java
@@ -2,6 +2,7 @@ package junit;
 
 import static org.junit.Assert.*;
 import jhlir.REngineException;
+import jhlir.RErrorException;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,9 +13,57 @@ public class REngineServices_Test extends MyTestCase {
     public void setUp() throws Exception {
         super.setUp();
     }
-
-    @Test(expected=REngineException.class)
-    public void testNullPointerException1() {        
-        getRServices().evalVoid("rnorm(100");        
+    
+    @Test
+    public void testWarnings() {
+    	getRServices().evalVoid("warning(\"Warning!\")");
+    	String warning = ""; // getRServices().getWarning();
+    	assertEquals("Warning", warning);
     }
+    
+    @Test
+    public void testErrors() {
+    	try {
+    		getRServices().evalVoid("stop(\"error\")");
+    	} catch(REngineException re) {
+    		String error = ""; // re.getErrorMessage();
+        	assertEquals("error", error);	
+    	}    	
+    }
+
+    @Test(expected=RErrorException.class)
+    public void testMissingBracket1() {        
+        getRServices().evalVoid("rnorm(100");
+        /* missing bracket ) */
+    }
+
+    @Test(expected=RErrorException.class)
+    public void testMissingBracket2() {        
+        getRServices().evalVoid("{rnorm(100)");
+        /* missing bracket } */
+    }
+    
+    @Test(expected=RErrorException.class)
+    public void testUnexpected() {        
+        getRServices().evalVoid("()");
+        /* Fehler: Unerwartetes ')' in "()" */
+    }    
+
+    @Test(expected=RErrorException.class)
+    public void testUnknownObject() {        
+        getRServices().evalVoid("unknown()");
+        /* Fehler: konnte Funktion "unknown" nicht finden */
+    }
+    
+    @Test(expected=RErrorException.class)
+    public void testunusedArguments() {        
+        getRServices().evalVoid("rnorm(x=100)");
+        /* Fehler in rnorm(x = 100) : unbenutzte(s) Argument(e) (x = 100) */
+    }
+    
+    @Test(expected=RErrorException.class)
+    public void testStop() {        
+        getRServices().evalVoid("stop(\"error\")");        
+    }
+    
 }
