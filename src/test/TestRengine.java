82759eb3d9c62a76ee0fc59cee22a9e5f55c33f1
berndbischl (Bernd Bischl) <bernd_bischl@gmx.net>
2009-08-31 16:58:26 +0000

    just some tests
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/jhlir@78 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/test/TestRengine.java
+++ src/test/TestRengine.java
@@ -1,28 +1,29 @@
 package test;
 
-import org.af.jhlir.backends.biocep.RCallServicesBiocep;
-import org.af.jhlir.call.RCallServices;
-import org.kchine.r.server.DirectJNI;
+import org.af.jhlir.backends.rengine.RCallServicesREngine;
 import org.rosuda.REngine.JRI.JRIEngine;
-import org.rosuda.REngine.REngine;
 
 public class TestRengine {
     public static void main(String[] args) {
 
-    	RCallServices rs = new RCallServicesBiocep(DirectJNI.getInstance().getRServices());
-    	
+
     	
     	
         try {
-//            Rengine re = new Rengine();
-            REngine re = new JRIEngine();
-            re.parseAndEval("xx <- list(1,2)");
-            re.parseAndEval("names(xx)[1] <- \"foo\"");
-            re.parseAndEval("yy <- list(foo=1,2)");
-            System.out.println(re.parseAndEval("xx").asList().keyAt(1));
-            System.out.println(re.parseAndEval("yy").asList().keyAt(1));
+            RCallServicesREngine rs = new RCallServicesREngine(new JRIEngine());
+            rs.engineEval("rnorm(10", true);
 
-
+////            Rengine re = new Rengine();
+//            REngine re = new JRIEngine();
+//            re.parseAndEval("xx <- list(1,2)");
+//            re.parseAndEval("names(xx)[1] <- \"foo\"");
+//            re.parseAndEval("yy <- list(foo=1,2)");
+//            System.out.println(re.parseAndEval("xx").asList().keyAt(1));
+//            System.out.println(re.parseAndEval("yy").asList().keyAt(1));
+//
+//            RTypeFactory rtf = RTypeFactoryREngine.getInstance(null);
+//
+//
 //            REXP r = re.parseAndEval("list()",g, false);
 //            System.out.println(r);
 ////            re.eval("plot(1:3)");
