4ee2c6ae54715e223fa77708698e23d85b8f390a
berndbischl (Bernd Bischl) <bernd_bischl@gmx.net>
2009-08-31 16:58:55 +0000

    more docs
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/jhlir@79 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/org/af/jhlir/tools/RCmdBatch.java
+++ src/org/af/jhlir/tools/RCmdBatch.java
@@ -51,7 +51,14 @@ public class RCmdBatch {
         }
     }
 
-
+    /**
+     * Excecute a list of R commands in batch mode.
+     * R CMD BATCH will write an output file, whose lines are returned as strings.
+     *
+     * @param input List of R commonds
+     * @return Contents of output file.
+     * @throws RCmdBatchException In case of IO errors or general errors with the R CMD BATCH process. Underlying exceptions are encapsulated as causing exceptions.
+     */
     public List<String> exec(List<String> input) throws RCmdBatchException {
         List<String> output = new ArrayList<String>();
         String fn = "r_batch_" + Calendar.getInstance().getTime().getTime();
@@ -124,30 +131,49 @@ public class RCmdBatch {
         p.close();
     }
 
-    // works only with one line print
-    public String retrieveInfo(List<String> input, String var) throws RCmdBatchException{
+
+    /**
+     * Excecute a list of R commands in batch mode and return the content of a R variable as a string.
+     * Uses exec method.
+     * !! Don't use this for too complicated stuff. At the moment the R variable must only generate a one-line string when printed!!
+     *
+     * @param input List of R commands
+     * @param var   R variable you want to retrieve.
+     * @return Contents R variable as string when printed in R.
+     * @throws RCmdBatchException In case of IO errors or general errors with the R CMD BATCH process. Underlying exceptions are encapsulated as causing exceptions.
+     */
+    public String retrieveInfo(List<String> input, String var) throws RCmdBatchException {
         return retrieveInfo(input, Arrays.asList(var)).get(var);
     }
 
-    // works only with one line print
-    public Map<String, String> retrieveInfo(List<String> input, List<String> vars) throws RCmdBatchException{
+    /**
+     * Excecute a list of R commands in batch mode and return the contents of R variables as strings.
+     * Uses exec method.
+     * !! Don't use this for too complicated stuff. At the moment the R variables must only generate a one-line string when printed!!
+     *
+     * @param input List of R commands
+     * @param vars  R variables you want to retrieve.
+     * @return Contents R variables as strings when printed in R.
+     * @throws RCmdBatchException In case of IO errors or general errors with the R CMD BATCH process. Underlying exceptions are encapsulated as causing exceptions.
+     */
+    public Map<String, String> retrieveInfo(List<String> input, List<String> vars) throws RCmdBatchException {
 
         Map<String, String> res = new HashMap<String, String>();
         List<String> is = new ArrayList<String>();
-        for (String v:vars) {
+        for (String v : vars) {
             is.add(v + "<- ''");
         }
         is.addAll(input);
-        for (String v:vars) {
+        for (String v : vars) {
             is.add("print(paste(\"<var>\"," + v + "))");
         }
         List<String> os = exec(is);
 
-        for (int i=0; i < vars.size(); i++) {
+        for (int i = 0; i < vars.size(); i++) {
             String s = os.get(os.size() - 1 - i);
             int n1 = s.indexOf("<var>");
             int n2 = s.lastIndexOf("\"");
-            res.put(vars.get(vars.size() - 1 - i), s.substring(n1+6, n2));
+            res.put(vars.get(vars.size() - 1 - i), s.substring(n1 + 6, n2));
         }
         return res;
     }
@@ -157,12 +183,19 @@ public class RCmdBatch {
     }
 
 
-
-    public RPackage getInstalledPackInfo(String p) throws RCmdBatchException {
+    /**
+     * Retrieve information about an installed R package on the current library paths.
+     * Uses exec method.
+     *
+     * @param pack R package name.
+     * @return Information about R package or null if package was not found.
+     * @throws RCmdBatchException See {@link #exec}.
+     */
+    public RPackage getInstalledPackInfo(String pack) throws RCmdBatchException {
         List<String> is = Arrays.asList(
-                "found <- require('" + p + "')",
+                "found <- require('" + pack + "')",
                 "if (found) {",
-                "desc <- packageDescription('" + p + "')",
+                "desc <- packageDescription('" + pack + "')",
                 "title <- desc$Title",
                 "ver <- desc$Version",
                 "lp <- dirname(dirname(dirname(attr(desc, 'file'))))",
@@ -173,9 +206,27 @@ public class RCmdBatch {
         Map<String, String> m = retrieveInfo(is, Arrays.asList("title", "lp", "ver"));
         if (m.get("lp").trim().equals(""))
             return null;
-        return new RPackage(p, m.get("title"), new File(m.get("lp")), m.get("ver"));
+        return new RPackage(pack, m.get("title"), new File(m.get("lp")), m.get("ver"));
     }
 
+    /**
+     * Check whether an R package was installed on the current library paths.
+     * Simply performs getInstalledPackInfo(pack) == null
+     *
+     * @param pack R package name.
+     * @return True if package was found.
+     * @throws RCmdBatchException See {@link #exec}.
+     */
+    public boolean isInstalled(String pack) throws RCmdBatchException {
+        return getInstalledPackInfo(pack) == null;
+    }
+
+    /**
+     * Retrieves all installed R packages on the library paths. Uses exec method.
+     *
+     * @return List of installed R package names.
+     * @throws RCmdBatchException See {@link #exec}.
+     */
     public List<String> getInstalledPackages() throws RCmdBatchException {
         List<String> is = Arrays.asList(
                 "ps <- installed.packages(noCache=TRUE)[,'Package']",
@@ -188,41 +239,75 @@ public class RCmdBatch {
     }
 
 
-    private RPackage installPackage(String p , String repos, File where) throws CantFindPackageException, RCmdBatchException {
+    private RPackage installPackage(String p, String repos, File where) throws CantFindPackageException, RCmdBatchException {
         List<String> is = Arrays.asList(
                 "install.packages('" +
-                p + "'" +
-                ", repos='" + repos + "'" +
-                (where == null ? "" : ", destdir='" + where.getAbsolutePath().replace("\\", "/") + "', lib='" + where.getAbsolutePath().replace("\\", "/") + "'") +
-                 ")"
+                        p + "'" +
+                        ", repos='" + repos + "'" +
+                        (where == null ? "" : ", destdir='" + where.getAbsolutePath().replace("\\", "/") + "', lib='" + where.getAbsolutePath().replace("\\", "/") + "'") +
+                        ")"
         );
         List<String> os = exec(is);
-        for (String s:os) {
+        for (String s : os) {
             if (s.contains(p) && s.contains("is not available"))
-                throw new CantFindPackageException("Can't find package " + p +" on repository " + repos + "!");
+                throw new CantFindPackageException("Can't find package " + p + " on repository " + repos + "!");
         }
         return getInstalledPackInfo(p);
     }
 
-    private RPackage installPackage(String p , String repos) throws CantFindPackageException, RCmdBatchException {
-        return installPackage(p, repos, null);
+    private RPackage installPackage(String pack, String repos) throws CantFindPackageException, RCmdBatchException {
+        return installPackage(pack, repos, null);
     }
 
 
-    public RPackage installCranPackage(String p) throws CantFindPackageException, RCmdBatchException {
-        return installPackage(p, "http://cran.r-project.org");
+    /**
+     * Installs an R package from CRAN into the defaullt library directory. Uses exec method.
+     *
+     * @param pack R package to install.
+     * @return Package information after install.
+     * @throws CantFindPackageException If the package is not available on CRAN.
+     * @throws RCmdBatchException       See {@link #exec}.
+     */
+    public RPackage installCranPackage(String pack) throws CantFindPackageException, RCmdBatchException {
+        return installPackage(pack, "http://cran.r-project.org");
     }
 
-    public RPackage installCranPackage(String p, File where) throws CantFindPackageException, RCmdBatchException {
-        return installPackage(p, "http://cran.r-project.org", where);
+    /**
+     * Installs an R package from CRAN. Uses exec method.
+     *
+     * @param pack  R package to install.
+     * @param where Directory to install the package into.
+     * @return Package information after install.
+     * @throws CantFindPackageException If the package is not available on CRAN.
+     * @throws RCmdBatchException       See {@link #exec}.
+     */
+    public RPackage installCranPackage(String pack, File where) throws CantFindPackageException, RCmdBatchException {
+        return installPackage(pack, "http://cran.r-project.org", where);
     }
 
-    public RPackage installRForgePackage(String p) throws CantFindPackageException, RCmdBatchException {
-        return installPackage(p, "http://R-Forge.R-project.org");
+    /**
+     * Installs an R package from R-Forge into the defaullt library directory. Uses exec method.
+     *
+     * @param pack R package to install.
+     * @return Package information after install.
+     * @throws CantFindPackageException If the package is not available on R-Forge.
+     * @throws RCmdBatchException       See {@link #exec}.
+     */
+    public RPackage installRForgePackage(String pack) throws CantFindPackageException, RCmdBatchException {
+        return installPackage(pack, "http://R-Forge.R-project.org");
     }
 
-    public RPackage installRForgePackage(String p, File where ) throws CantFindPackageException, RCmdBatchException {
-        return installPackage(p, "http://R-Forge.R-project.org", where);
+    /**
+     * Installs an R package from R-Forge. Uses exec method.
+     *
+     * @param pack  R package to install.
+     * @param where Directory to install the package into.
+     * @return Package information after install.
+     * @throws CantFindPackageException If the package is not available on R-Forge.
+     * @throws RCmdBatchException       See {@link #exec}.
+     */
+    public RPackage installRForgePackage(String pack, File where) throws CantFindPackageException, RCmdBatchException {
+        return installPackage(pack, "http://R-Forge.R-project.org", where);
     }
 
 
@@ -269,10 +354,10 @@ public class RCmdBatch {
 
     /**
      * Collects information of the used R engine and stores it in this object.
+     *
      * @throws RCmdBatchException
      * @see this.getRVersion
      * @see this.getLibPaths
-     *
      */
     public void retrieveRInfo() throws RCmdBatchException {
         List<String> input = new ArrayList<String>();
@@ -303,22 +388,6 @@ public class RCmdBatch {
     public String getLibPaths() {
         return libPaths;
     }
-
-
-//    protected String getRJavaPath() throws IOException, InterruptedException{
-//
-//        RExecutor rExecutor = RExecutor.getRExecutor(rHomeDir.getAbsolutePath());
-//
-//        List<String> input = Arrays.asList(
-//               "installed.packages(noCache=TRUE)['rJava','LibPath']"
-//        );
-//        ArrayList<String> output = new ArrayList();
-//
-//        rExecutor.exec(input, output);
-//        Logger.log(output.get(0));
-//        Logger.log(output.get(1));
-//        return output.get(0);
-//    }
 }
 
 
